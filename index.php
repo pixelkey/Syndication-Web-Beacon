88b930e97146896877cd567b638b19b16126766c
Andrew Greirson <andrew@pixelkey.com.au>
2021-08-17 11:56:59 +0930

    Filter out slug from referrer site

--- index.php
+++ index.php
@@ -12,7 +12,13 @@ use GeoIp2\Exception\GeoIp2Exception;
 $user_id = $_GET['id'];
 $title = html_entity_decode(urldecode($_GET['title']));
 $visit_date = date('Y-m-d H:i:s');
-$site = $_SERVER['HTTP_REFERER'];
+$site = '';
+
+$refererUrl = $_SERVER['HTTP_REFERER'];
+if ($urlSections = parse_url($refererUrl)) {
+  $site = $urlSections["scheme"] . "://" . $urlSections["host"];
+}
+
 $user_ip = $_SERVER['REMOTE_ADDR'];
 $user_agent = $_SERVER['HTTP_USER_AGENT'];
 
